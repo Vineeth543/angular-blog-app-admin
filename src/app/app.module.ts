ad092be77247d15c651c8ee774e316efa2618944
Vineeth Serigar <vineeth.serigar@netcracker.com>
2023-05-18 15:36:19 +0530

    designed dashboard & firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { FirestoreModule } from '@angular/fire/firestore';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { environment } from 'src/environments/environment.prod';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    CategoriesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    FirestoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC1VkGTAFEIqFiMdse4vREVB8Xt2ymlcYY',
+    authDomain: 'angular-blog-app-90595.firebaseapp.com',
+    projectId: 'angular-blog-app-90595',
+    storageBucket: 'angular-blog-app-90595.appspot.com',
+    messagingSenderId: '403458713390',
+    appId: '1:403458713390:web:28faedb67d02f64fd63cbf',
+  },
+};
