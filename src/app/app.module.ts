b2aeaee98c4776ff23808983644abb99fab180a9
Vineeth Serigar <vineeth.serigar@netcracker.com>
2023-05-19 16:46:05 +0530

    category form validation & firestore setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AngularFireModule } from '@angular/fire/compat';
-import { FirestoreModule } from '@angular/fire/firestore';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +11,7 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { environment } from 'src/environments/environment';
 import { CategoriesComponent } from './categories/categories.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { CategoriesComponent } from './categories/categories.component';
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    FirestoreModule,
+    AngularFirestoreModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.less']
+  styleUrls: ['./categories.component.less'],
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  constructor(private afs: AngularFirestore) {}
 
+  ngOnInit(): void {}
+
+  onSubmit(formData: NgForm): void {
+    let subcategoriesData = {
+      subCategory: 'subCategory2',
+    };
+    this.afs
+      .collection('categories')
+      .add(formData.value)
+      .then((docRef) => {
+        console.log('Document written: ', docRef);
+        this.afs
+          .collection('categories')
+          .doc(docRef.id)
+          .collection('subcategories')
+          .add(subcategoriesData)
+          .then((docRef) => console.log('Document written: ', docRef));
+      })
+      .catch((error) => console.error('Error adding document: ', error));
+  }
 }
