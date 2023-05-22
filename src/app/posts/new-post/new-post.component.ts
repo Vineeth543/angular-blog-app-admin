57964e6b8b2b6759660d12bc52ce79abb2e0abd7
Vineeth Serigar <vineeth.serigar@netcracker.com>
2023-05-22 14:30:28 +0530

    generate permalink

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.less']
+  styleUrls: ['./new-post.component.less'],
 })
 export class NewPostComponent {
+  permalink!: string;
 
+  generatePermalink($event: Event): void {
+    const title = ($event.target as HTMLInputElement).value;
+    this.permalink = title
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9]+/g, '-')
+      .replace(/^-+/, '')
+      .replace(/-+$/, '');
+  }
 }
